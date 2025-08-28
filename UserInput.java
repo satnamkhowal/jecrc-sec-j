f93164bf9b22a8931a41e934cdd9cb04057b9a78
Satnam Singh <singhkhowal@gmail.com>
2025-08-28 09:21:29 +0530

    'second'

---
+++ UserInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UserInput{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the First Number : ");
+		int a=sc.nextInt();
+		System.out.println("Enter the First Number : ");
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("Addition ="+c);
+	}
+}
